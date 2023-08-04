b986251cd36c36779ba254f7a9a573193f3df6af
jinha <jinha5963@naver.com>
2023-08-04 20:54:21 +0900

    feat : 회원가입 api  data/user.json 파일로 관리하도록 수정

--- backend/DongUk/routes/register.js
+++ backend/DongUk/routes/register.js
@@ -1,15 +1,31 @@
-// routes/register.js
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const path = require("path");
 
-// Sample in-memory user data (for demonstration purposes)
-const users = [];
+const storagePath = path.join(__dirname, "../data");
+const userFilePath = path.join(storagePath, "user.json");
+// Function to read users from the JSON file
+const readUsers = () => {
+  try {
+    const usersJson = fs.readFileSync(userFilePath, "utf8");
+    console.log("before" + JSON.parse(usersJson));
+    return JSON.parse(usersJson);
+  } catch (error) {
+    console.error("Error reading users from file:", error.message);
+    return [];
+  }
+};
 
 // Registration route
 router.post("/", (req, res) => {
-  const { username, password, nickname, userID } = req.body;
+  const { username, password, nickname, userID, likeOTTs, likeGenres } =
+    req.body;
 
-  // Check if the username already exists in the users array
+  // Read existing users from the JSON file
+  const users = readUsers();
+  console.log("newusers", users);
+  // Check if the userID already exists in the users array
   const existingUser = users.find((user) => user.userID === userID);
   if (existingUser) {
     return res
@@ -17,11 +33,34 @@ router.post("/", (req, res) => {
       .json({ isRegistered: false, error: "UserID already exists" });
   }
 
-  // Save the user data in the users array (replace this with a database insert in a real application)
-  users.push({ username, password, nickname, userID });
+  // Save the new user in the users array
+  const newUser = {
+    username,
+    password,
+    nickname,
+    userID,
+    likeOTTs,
+    likeGenres,
+  };
+  users.push(newUser);
 
-  // Registration successful
-  return res.json({ isRegistered: true, message: "Registration successful" });
+  // Write the updated users array to the JSON file
+  fs.writeFile(userFilePath, JSON.stringify(users), (err) => {
+    if (err) {
+      console.error("Error saving user data to file:", err.message);
+      return res
+        .status(500)
+        .json({ isRegistered: false, error: "Error saving user data" });
+    } else {
+      console.log("User data saved to user.json successfully.");
+      console.log("after " + JSON.stringify(users));
+      // Registration successful
+      return res.json({
+        isRegistered: true,
+        message: "Successful registration",
+      });
+    }
+  });
 });
 
 module.exports = router;
