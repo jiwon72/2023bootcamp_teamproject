010df3438ac7be60b60aa0a9a8c6044fea042a19
jinha <jinha5963@naver.com>
2023-08-05 17:13:37 +0900

    refactor : session 체크코드 분리

--- backend/DongUk/routes/setDark.js
+++ backend/DongUk/routes/setDark.js
@@ -24,45 +24,44 @@ router.post("/", (req, res) => {
   //session에 user가 있는지 확인
   const sessionUser = req.session.user;
   // 유저 목록 불러오기
-  let users = readUsers();
-  if (sessionUser) {
-    //users.json(유저목록)에서 user가 있는지 확인
-
-    const user = users.find((user) => user.userID === sessionUser.userID);
-    if (user) {
-      // Update the user's isDark property
-      for (let i = 0; i < users.length; i++) {
-        //users.json(유저목록)에서 user가 있는지 확인
-        if (users[i].userID === sessionUser.userID) {
-          console.log("userID" + users[i].userID);
-          console.log("isDark" + users[i].isDark);
-          users[i].isDark = req.body.isDark;
-          console.log("users" + JSON.stringify(users));
-
-          // Write the updated users array to the JSON file
-          fs.writeFile(userFilePath, JSON.stringify(users), (err) => {
-            if (err) {
-              console.error("Error saving user data to file:", err.message);
-              return res.status(500).json({ error: "Error saving user data" });
-            } else {
-              console.log("user" + user + " " + "isDark" + user.isDark);
-              // Return the updated user
-              return res.json({
-                isDark: user.isDark,
-                message: "Darkmode data updated successfully",
-              });
-            }
-          });
-        }
-      }
-    } else {
-      return res.status(400).json({ error: "User not found" });
-    }
-  } else {
-    //session에 user가 없다면 로그아웃 된 상태
+  //session에 user가 없다면 오류 발생
+  if (!sessionUser) {
     return res
       .status(401)
       .json({ isLoggedIn: false, message: "session expired login again" });
+  }
+
+  //users.json(유저목록)에서 user가 있는지 확인
+  let users = readUsers();
+  const user = users.find((user) => user.userID === sessionUser.userID);
+  if (user) {
+    // Update the user's isDark property
+    for (let i = 0; i < users.length; i++) {
+      //users.json(유저목록)에서 user가 있는지 확인
+      if (users[i].userID === sessionUser.userID) {
+        console.log("userID" + users[i].userID);
+        console.log("isDark" + users[i].isDark);
+        users[i].isDark = req.body.isDark;
+        console.log("users" + JSON.stringify(users));
+
+        // Write the updated users array to the JSON file
+        fs.writeFile(userFilePath, JSON.stringify(users), (err) => {
+          if (err) {
+            console.error("Error saving user data to file:", err.message);
+            return res.status(500).json({ error: "Error saving user data" });
+          } else {
+            console.log("user" + user + " " + "isDark" + user.isDark);
+            // Return the updated user
+            return res.json({
+              isDark: user.isDark,
+              message: "Darkmode data updated successfully",
+            });
+          }
+        });
+      }
+    }
+  } else {
+    return res.status(400).json({ error: "User not found" });
   }
 });
 
