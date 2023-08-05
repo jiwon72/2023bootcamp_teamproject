3097a610cd1ef2a0d9d636df80004637e8b53243
jinha <jinha5963@naver.com>
2023-08-05 17:08:16 +0900

    chore : isDark api 주석 변경 및 post=>get

--- backend/DongUk/routes/isDark.js
+++ backend/DongUk/routes/isDark.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const storagePath = path.join(__dirname, "../data");
 const userFilePath = path.join(storagePath, "user.json");
-// Function to read users from the JSON file
+// 유저 목록 불러오는 함수
 const readUsers = () => {
   try {
     const usersJson = fs.readFileSync(userFilePath, "utf8");
@@ -19,15 +19,18 @@ const readUsers = () => {
 };
 
 // Logout route
-router.post("/", (req, res) => {
-  //check if user is logged in
+router.get("/", (req, res) => {
+  //session에 user가 있는지 확인
   const sessionUser = req.session.user;
   console.log(JSON.stringify(sessionUser));
+  // 유저 목록 불러오기
   const users = readUsers();
   if (sessionUser) {
+    //users.json(유저목록)에서 user가 있는지 확인
     const user = users.find((user) => user.userID === sessionUser.userID);
     return res.json({ isDark: user.isDark });
   } else {
+    //session에 user가 없다면 로그아웃 된 상태
     return res
       .status(401)
       .json({ isLoggedIn: false, message: "session expired login again" });
