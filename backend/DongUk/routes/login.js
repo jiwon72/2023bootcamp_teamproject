62c102e459218ee3b46d940b301b566f0ed4cdb2
jinha <jinha5963@naver.com>
2023-08-05 17:09:12 +0900

    feat : login 시 user.json파일에서 정보 읽어오는 코드

--- backend/DongUk/routes/login.js
+++ backend/DongUk/routes/login.js
@@ -2,13 +2,26 @@
 const express = require("express");
 const router = express.Router();
 const { setCookie } = require("../helpers/cookieHelper"); // Import cookie helper function
+const fs = require("fs");
+const path = require("path");
+
+const storagePath = path.join(__dirname, "../data");
+const userFilePath = path.join(storagePath, "user.json");
 
 // Sample in-memory user data (for demonstration purposes)
-const users = [
-  { userID: "user1", password: "password1" },
-  { userID: "user2", password: "password2" },
-];
+const readUsers = () => {
+  try {
+    const usersJson = fs.readFileSync(userFilePath, "utf8");
+    console.log("before" + JSON.parse(usersJson));
+    return JSON.parse(usersJson);
+  } catch (error) {
+    console.error("Error reading users from file:", error.message);
+    return [];
+  }
+};
+const users = readUsers(); // Function to read users from the JSON file
 
+console.log("readUsers" + users);
 // Login route
 router.post("/", (req, res) => {
   const { userID, password } = req.body;
@@ -23,6 +36,7 @@ router.post("/", (req, res) => {
 
   // User authentication successful. Set a session flag to indicate that the user is logged in.
   req.session.user = { userID: userID };
+  console.log(req.session.user);
   // Respond with a success message.
   return res.json({ isLoggedIn: true, message: "Login successful" });
 });
