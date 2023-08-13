37722d244e76fdbb1d4efc154522603de63512df
jinha <jinha5963@naver.com>
2023-08-14 02:19:46 +0900

    feat : login.js DB연결 (주석처리함)

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { setCookie } = require("../helpers/cookieHelper"); // Import cookie helper function
 const fs = require("fs");
 const path = require("path");
+const getConnection = require("../data/DBpool.js");
 
 const storagePath = path.join(__dirname, "../data");
 const userFilePath = path.join(storagePath, "user.json");
@@ -25,7 +26,8 @@ console.log("readUsers" + users);
 // Login route
 router.post("/", (req, res) => {
   const { userID, password } = req.body;
-  // Find the user with matching credentials in the array (replace this with a database query in a real application)
+
+  //파일을 통한 로그인 구현 DB사용시 주석처리
   const user = users.find(
     (user) => user.userID === userID && user.password === password
   );
@@ -39,6 +41,30 @@ router.post("/", (req, res) => {
   console.log(req.session.user);
   // Respond with a success message.
   return res.json({ isLoggedIn: true, message: "Login successful" });
+
+  //DB를 위한 코드 사용 시 위 코드 주석 처리
+  // const query = "SELECT * FROM user WHERE user_ID = ? AND user_Password = ?";
+  // const values = [userID, password];
+  // const db = getConnection((conn) =>
+  //   conn.query(query, values, (err, result) => {
+  //     conn.release();
+  //     if (err) {
+  //       console.log(err);
+  //       res.status(500).send("An error occurred during login");
+  //       return;
+  //     }
+  //     if (result.length === 0) {
+  //       // No user found with matching credentials
+  //       res.status(401).json({ isLoggedIn: false, message: "Login fail" });
+  //     } else {
+  //       // User authentication successful. Set a session flag to indicate that the user is logged in.
+  //       req.session.user = { userID: userID };
+  //       console.log(req.session.user);
+  //       // Respond with a success message.
+  //       res.json({ isLoggedIn: true, message: "Login successful" });
+  //     }
+  //   })
+  // );
 });
 
 module.exports = router;
