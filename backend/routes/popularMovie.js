c38cfb9570ffaf8c61862ecc5bdcff2f80216881
jinha <jinha5963@naver.com>
2023-08-18 21:19:34 +0900

    feat : 장르별 인기영화 조회 api 추가

--- backend/routes/popularMovie.js
+++ backend/routes/popularMovie.js
@@ -5,8 +5,6 @@ const router = express.Router();
 TMDB_ACCESS_TOKEN = process.env.TMDB_ACCESS_TOKEN;
 
 router.get("/popular", async (req, res) => {
-  console.log(TMDB_ACCESS_TOKEN);
-
   const options = {
     method: "GET",
     url: "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
@@ -23,6 +21,28 @@ router.get("/popular", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).send("Error fetching latest movies");
+  }
+});
+
+router.get("/popular/:genre", async (req, res) => {
+  const genreID = req.params.genre; // Get the genre ID from the route parameter
+
+  const options = {
+    method: "GET",
+    url: `https://api.themoviedb.org/3/movie/popular?with_genres=${genreID}&language=ko-KR&page=1`,
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    const movies = response.data.results.slice(0, 10);
+    res.json(movies);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching movies for the given genre");
   }
 });
 
