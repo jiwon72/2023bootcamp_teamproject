1c273182a5870d540b04e3dc72ff72d84a8710ca
jinha <jinha5963@naver.com>
2023-08-22 14:24:38 +0900

    feat : review db코드 연결결

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -4,76 +4,187 @@ const path = require("path");
 const router = express.Router();
 const storagePath = path.join(__dirname, "../data");
 const reviewFilePath = path.join(storagePath, "review.json");
+const getConnection = require("../data/DBpool.js");
 
 // Edit Review
+// router.post("/reviews/:reviewId", (req, res) => {
+//   const reviews = JSON.parse(fs.readFileSync(reviewFilePath, "utf8"));
+//   if (!req.session.user) {
+//     return res.status(401).json({ message: "Session expired. Please login." });
+//   }
+//   const userID = req.session.user.userID;
+//   const { content, rating } = req.body;
+//   const reviewId = req.params.reviewID;
+
+//   const index = reviews.findIndex((review) => review.id === parseInt(reviewId));
+//   if (index === -1) {
+//     return res.status(404).json({ message: "Review not found." });
+//   }
+
+//   reviews[index].content = content;
+//   reviews[index].rating = rating;
+
+//   fs.writeFileSync(reviewFilePath, JSON.stringify(reviews));
+//   res.json({ message: "Review updated successfully.", isEdit: true });
+// });
+
+// Edit Review from DB
 router.post("/reviews/:reviewId", (req, res) => {
-  const reviews = JSON.parse(fs.readFileSync(reviewFilePath, "utf8"));
   if (!req.session.user) {
     return res.status(401).json({ message: "Session expired. Please login." });
   }
-  const userID = req.session.user.userID;
   const { content, rating } = req.body;
   const reviewId = req.params.reviewId;
 
-  const index = reviews.findIndex((review) => review.id === parseInt(reviewId));
-  if (index === -1) {
-    return res.status(404).json({ message: "Review not found." });
-  }
+  const query = "UPDATE reviews SET content = ?, rating = ? WHERE id = ?";
+  const values = [content, rating, reviewId];
 
-  reviews[index].content = content;
-  reviews[index].rating = rating;
-
-  fs.writeFileSync(reviewFilePath, JSON.stringify(reviews));
-  res.json({ message: "Review updated successfully." });
+  getConnection((err, conn) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("An error occurred");
+    }
+    conn.query(query, values, (err) => {
+      conn.release();
+      if (err) {
+        console.log(err);
+        return res.status(500).send("An error occurred");
+      }
+      res.json({ message: "Review updated successfully." });
+    });
+  });
 });
 
 // Delete Review
+// router.delete("/reviews/:reviewId", (req, res) => {
+//   const reviews = JSON.parse(fs.readFileSync(reviewFilePath, "utf8"));
+//   const reviewId = req.params.reviewId;
+
+//   const newReviews = reviews.filter(
+//     (review) => review.id !== parseInt(reviewId)
+//   );
+//   fs.writeFileSync(reviewFilePath, JSON.stringify(newReviews));
+
+//   res.json({ message: "Review deleted successfully.", isDelete: true });
+// });
+
+// Delete Review from DB
 router.delete("/reviews/:reviewId", (req, res) => {
-  const reviews = JSON.parse(fs.readFileSync(reviewFilePath, "utf8"));
   const reviewId = req.params.reviewId;
 
-  const newReviews = reviews.filter(
-    (review) => review.id !== parseInt(reviewId)
-  );
-  fs.writeFileSync(reviewFilePath, JSON.stringify(newReviews));
-
-  res.json({ message: "Review deleted successfully." });
+  const query = "DELETE FROM Review WHERE ReviewID = ?";
+  getConnection((conn, err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("An error occurred");
+    }
+    conn.query(query, [reviewId], (err, result) => {
+      // Check if any rows were affected
+      if (result.affectedRows === 0) {
+        return res.status(404).send("No review found with the given ID");
+      }
+      conn.release();
+      if (err) {
+        console.log(err);
+        return res.status(500).send("An error occurred");
+      }
+      return res.json({
+        message: "Review deleted successfully.",
+        isDelete: true,
+      });
+    });
+  });
 });
 
 // Lookup Reviews
+// router.get("/movies/:movieId/reviews", (req, res) => {
+//   const reviews = JSON.parse(fs.readFileSync(reviewFilePath, "utf8"));
+//   const movieId = req.params.movieId;
+
+//   const filteredReviews = reviews.filter(
+//     (review) => review.movieId === parseInt(movieId)
+//   );
+//   res.json({ reviews: filteredReviews });
+// });
+
+//lookup reviews from DB
 router.get("/movies/:movieId/reviews", (req, res) => {
-  const reviews = JSON.parse(fs.readFileSync(reviewFilePath, "utf8"));
   const movieId = req.params.movieId;
 
-  const filteredReviews = reviews.filter(
-    (review) => review.movieId === parseInt(movieId)
-  );
-  res.json({ reviews: filteredReviews });
+  const query = "SELECT * FROM Review WHERE Movie_Movie_ID = ?";
+  getConnection((conn, err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("An error occurred ");
+    }
+    conn.query(query, [movieId], (err, results) => {
+      conn.release();
+      if (err) {
+        console.log(err.message);
+        return res.status(500).send("An error occurred during query execution");
+      }
+      res.json({ reviews: results });
+    });
+  });
 });
 
-// Write Review
+// // Write Review
+// router.post("/movies/:movieId/reviews", (req, res) => {
+//   if (!req.session.user) {
+//     return res.status(401).json({ message: "Session expired. Please login." });
+//   }
+//   const reviews = JSON.parse(fs.readFileSync(reviewFilePath, "utf8"));
+//   const { rating, content, isSpoil } = req.body;
+//   const createDate = new Date().toISOString().slice(0, 10);
+
+//   const newReview = {
+//     id: reviews.length + 1, // Simple auto-increment logic. Adjust as per your needs.
+//     userID: req.session.user.userID,
+//     movieId: parseInt(req.params.movieId),
+//     rating,
+//     content,
+//     isSpoil,
+//     createDate,
+//   };
+
+//   reviews.push(newReview);
+//   fs.writeFileSync(reviewFilePath, JSON.stringify(reviews));
+
+//   res.json({ message: "Review added successfully." });
+// });
+// Write Review from DB
 router.post("/movies/:movieId/reviews", (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ message: "Session expired. Please login." });
   }
-  const reviews = JSON.parse(fs.readFileSync(reviewFilePath, "utf8"));
   const { rating, content, isSpoil } = req.body;
   const createDate = new Date().toISOString().slice(0, 10);
 
-  const newReview = {
-    id: reviews.length + 1, // Simple auto-increment logic. Adjust as per your needs.
-    userID: req.session.user.userID,
-    movieId: parseInt(req.params.movieId),
+  const query =
+    "INSERT INTO Review (user_userID, Movie_Movie_ID, rating, content, isSpoil, createDate) VALUES (?, ?, ?, ?, ?, NOW())";
+  const values = [
+    req.session.user.userID,
+    req.params.movieId,
     rating,
     content,
     isSpoil,
     createDate,
-  };
+  ];
 
-  reviews.push(newReview);
-  fs.writeFileSync(reviewFilePath, JSON.stringify(reviews));
-
-  res.json({ message: "Review added successfully." });
+  const db = getConnection((conn, err) => {
+    if (err) {
+      console.error("Database connection error:", err);
+      return res.status(500).send("An error occurred");
+    }
+    conn.query(query, values, (err) => {
+      conn.release();
+      if (err) {
+        console.log(err.message);
+        return res.status(500).send("An error occurred");
+      }
+      res.json({ message: "Review added successfully." });
+    });
+  });
 });
 
 module.exports = router;
