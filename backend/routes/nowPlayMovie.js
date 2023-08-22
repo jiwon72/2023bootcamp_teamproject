337722208d0859f0575d20d09af3dfdacb6a9ec0
jinha <jinha5963@naver.com>
2023-08-22 22:23:04 +0900

    feat : 최신영화 출력 갯수 제한 해제

--- backend/routes/nowPlayMovie.js
+++ backend/routes/nowPlayMovie.js
@@ -5,11 +5,10 @@ const router = express.Router();
 TMDB_ACCESS_TOKEN = process.env.TMDB_ACCESS_TOKEN;
 
 router.get("/latest", async (req, res) => {
-  console.log(TMDB_ACCESS_TOKEN);
-
+  const page = req.query.page || 1;
   const options = {
     method: "GET",
-    url: "https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1",
+    url: `https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=${page}`,
     headers: {
       accept: "application/json",
       Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
@@ -18,7 +17,7 @@ router.get("/latest", async (req, res) => {
 
   try {
     const response = await axios.request(options);
-    const movies = response.data.results.slice(0, 10);
+    const movies = response.data.results;
     res.json(movies);
   } catch (error) {
     console.error(error);
