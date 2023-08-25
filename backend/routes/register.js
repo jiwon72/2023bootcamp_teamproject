43afd94afc51905cbfab75ec3c7384afc0dd3831
jinha <jinha5963@naver.com>
2023-08-25 19:20:10 +0900

    feat : 회원가입 DB연결

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -55,61 +55,61 @@ router.post("/", (req, res) => {
     likeGenres,
   } = req.body;
 
-  // Read existing users from the JSON file
-  const users = readUsers();
-  console.log("newusers", users);
-  // Check if the userID already exists in the users array
-  const existingUser = users.find((user) => user.userID === userID);
-  if (existingUser) {
-    return res
-      .status(409)
-      .json({ isRegistered: false, error: "UserID already exists" });
-  }
+  // // Read existing users from the JSON file
+  // const users = readUsers();
+  // console.log("newusers", users);
+  // // Check if the userID already exists in the users array
+  // const existingUser = users.find((user) => user.userID === userID);
+  // if (existingUser) {
+  //   return res
+  //     .status(409)
+  //     .json({ isRegistered: false, error: "UserID already exists" });
+  // }
 
-  // Save the new user in the users array
-  const newUser = {
-    username,
-    password,
-    nickname,
-    userID,
-    likeOTTs,
-    likeGenres,
-    birthdate,
-  };
-  users.push(newUser);
+  // // Save the new user in the users array
+  // const newUser = {
+  //   username,
+  //   password,
+  //   nickname,
+  //   userID,
+  //   likeOTTs,
+  //   likeGenres,
+  //   birthdate,
+  // };
+  // users.push(newUser);
 
-  // Write the updated users array to the JSON file
-  fs.writeFile(userFilePath, JSON.stringify(users), (err) => {
-    if (err) {
-      console.error("Error saving user data to file:", err.message);
-      return res
-        .status(500)
-        .json({ isRegistered: false, error: "Error saving user data" });
-    } else {
-      console.log("User data saved to user.json successfully.");
-      console.log("after " + JSON.stringify(users));
-      // Registration successful
-      return res.json({
-        isRegistered: true,
-        message: "Successful registration",
-      });
-    }
-  });
+  // // Write the updated users array to the JSON file
+  // fs.writeFile(userFilePath, JSON.stringify(users), (err) => {
+  //   if (err) {
+  //     console.error("Error saving user data to file:", err.message);
+  //     return res
+  //       .status(500)
+  //       .json({ isRegistered: false, error: "Error saving user data" });
+  //   } else {
+  //     console.log("User data saved to user.json successfully.");
+  //     console.log("after " + JSON.stringify(users));
+  //     // Registration successful
+  //     return res.json({
+  //       isRegistered: true,
+  //       message: "Successful registration",
+  //     });
+  //   }
+  // });
 
   //use DataBase
-  // const query =
-  //   "INSERT INTO user (user_Name, user_Password, NickName, birthday, user_ID) VALUES (?, ?, ?, ?, ?)";
-  // const values = [username, password, nickname, birthdate, userID];
-  // const db = getConnection((conn) =>
-  //   conn.query(query, values, (err, result) => {
-  //     if (err) {
-  //       console.log(err);
-  //       res.status(500).send("An error occurred");
-  //       return;
-  //     }
-  //     res.status(200).send("User registered");
-  //   })
-  // );
+  const query =
+    "INSERT INTO user (user_Name, user_Password, NickName, birthday, user_ID) VALUES (?, ?, ?, ?, ?)";
+  const values = [username, password, nickname, birthdate, userID];
+  const db = getConnection((conn) =>
+    conn.query(query, values, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("An error occurred");
+        return;
+      }
+      res.status(200).send("User registered");
+    })
+  );
 });
 
 module.exports = router;
