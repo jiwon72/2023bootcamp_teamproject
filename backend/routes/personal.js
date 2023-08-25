a942d81f258c039b89444b0524e7cc99bf0786fb
iamjinwon <a67841586@gmail.com>
2023-08-25 14:38:20 +0900

    jinwon

--- backend/routes/personal.js
+++ backend/routes/personal.js
@@ -1,35 +1,45 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
-const path = require("path");
 
-const userDataPath = path.join(__dirname, "../data/user.json");
+const getConnection = require("../data/DBpool.js");
 
 // Define the route for getting user profiles
-router.get("/:userId/profile", (req, res) => {
-  const userId = req.params.userId;
-  console.log(`Requested userId: ${userId}`);
+router.get("/profile", (req, res) => {
+  const userSession = req.session.user;
+  console.log(req.session);
 
-  fs.readFile(userDataPath, "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading user data file:", err.message);
-      return res.status(500).json({ error: "Internal server error" });
-    }
+  // 로그인 상태 확인
+  if (!userSession) {
+    return res.status(401).json({
+      isSuccess: false,
+      message: "Session expired. Please log in.",
+    });
+  }
 
-    const users = JSON.parse(data);
-    const user = users.find((user) => user.userID === userId);
+  const userId = userSession.userID;
 
-    if (!user) {
-      console.log(`User not found for userId: ${userId}`);
-      return res.status(404).json({ error: "User not found" });
-    }
+  getConnection(function (conn) {
+    const selectQuery = "SELECT NickName, user_Password FROM user WHERE user_ID = ?";
 
-    const { userID, username, email, nickname } = user;
-    console.log(`Found user:`, user);
-    res.json({ userID, username, email, nickname });
+    conn.query(selectQuery, [userId], (error, results) => {
+      conn.release(); // 연결 해제
+
+      if (error) {
+        console.error("Error querying the database:", error);
+        return res.status(500).json({ error: "Internal server error" });
+      }
+
+      if (results.length === 0) {
+        console.log(`User not found for userId: ${userId}`);
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      const user = results[0];
+      const { NickName, user_Password } = user;
+      console.log(`Found user:`, user);
+      res.json({ NickName, user_Password });
+    });
   });
 });
-
-
 
 module.exports = router;
--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -5,7 +5,52 @@ const router = express.Router();
 const storagePath = path.join(__dirname, "../data");
 const questionFilePath = path.join(storagePath, "questions.json");
 
-// Get List of Questions
+const getConnection = require("../data/DBpool.js");
+
+// 질문 목록 조회
+router.get("/questions", (req, res) => {
+  getConnection(function (conn) {
+    const selectQuery = "SELECT * FROM Question";
+
+    conn.query(selectQuery, function (error, results) {
+      conn.release();
+      if (error) {
+        console.error("Error executing query:", error);
+        res.status(500).json({ error: "An error occurred while retrieving questions." });
+        return;
+      }
+      res.json({ questions: results });
+    });
+  });
+});
+
+
+// 특정 질문 조회
+router.get("/questions/:Question_ID", (req, res) => {
+  const questionId = req.params.Question_ID;
+
+  getConnection(function (conn) {
+    const selectQuery = "SELECT * FROM Question WHERE Question_ID = ?";
+
+    conn.query(selectQuery, [questionId], function (error, results) {
+      conn.release();
+      if (error) {
+        console.error("Error executing query:", error);
+        res.status(500).json({ error: "An error occurred while retrieving the question." });
+        return;
+      }
+
+      if (results.length === 0) {
+        res.status(404).json({ error: "Question not found." });
+        return;
+      }
+
+      res.json({ question: results[0] });
+    });
+  });
+});
+
+/* // Get List of Questions
 router.get("/questions", (req, res) => {
   const questions = JSON.parse(fs.readFileSync(questionFilePath, "utf8"));
   res.json(questions);
@@ -22,27 +67,163 @@ router.get("/questions/:questionId", (req, res) => {
   }
 
   res.json(question);
-});
+}); */
 
-// Write Your Questions
+// // Write Your Questions
+// router.post("/questions", (req, res) => {
+// const questions = JSON.parse(fs.readFileSync(questionFilePath, "utf8"));
+// const { title, content } = req.body;
+
+// const newQuestion = {
+//     id: questions.length + 1, // Simple auto-increment logic. Adjust as per your needs.
+//     title,
+//     content,
+//     NickName 
+//   };
+//   questions.push(newQuestion);
+//   fs.writeFileSync(questionFilePath, JSON.stringify(questions));
+
+//   res.json({ message: "Question added successfully.", ...newQuestion });
+// });
+
+// 질문추가
 router.post("/questions", (req, res) => {
-  const questions = JSON.parse(fs.readFileSync(questionFilePath, "utf8"));
-  const { title, content, nickname } = req.body;
+  const userSession = req.session.user;
 
-  const newQuestion = {
-    id: questions.length + 1, // Simple auto-increment logic. Adjust as per your needs.
-    title,
-    content,
-    nickname // This is just for the sake of the example. You might want to get the nickname from the session or body.
-  };
+  if (!userSession) {
+    return res.status(401).json({
+      isSuccess: false,
+      message: "Session expired. Please log in.",
+    });
+  }
 
-  questions.push(newQuestion);
-  fs.writeFileSync(questionFilePath, JSON.stringify(questions));
+  const userId = userSession.userID; // 세션에서 userID 가져오기
+  const { title, content, NickName, Answer } = req.body; // user_user_ID는 세션에서 가져옴
 
-  res.json({ message: "Question added successfully.", ...newQuestion });
+  getConnection(function (conn) {
+    const insertQuery = `
+      INSERT INTO Question
+      (content, CreateDate, title, user_user_ID, NickName, Answer)
+      VALUES (?, NOW(), ?, ?, ?, ?)
+    `;
+
+    conn.query(
+      insertQuery,
+      [content, title, userId, NickName, Answer], // userID는 세션에서 가져온 값으로
+      function (error, results) {
+        conn.release();
+        if (error) {
+          console.error("Error executing query:", error);
+          res.status(500).json({ error: "An error occurred while adding the question." });
+          return;
+        }
+
+        const newQuestion = {
+          Question_ID: results.insertId,
+          content,
+          CreateDate: new Date().toISOString(),
+          title,
+          user_user_ID: userId, // userID는 세션에서 가져온 값으로
+          NickName,
+          Answer,
+        };
+
+        res.json({ message: "Question added successfully.", ...newQuestion });
+      }
+    );
+  });
 });
 
-// Edit Question
+
+// 질문 수정
+router.post("/questions/:Question_ID", (req, res) => {
+  const questionId = req.params.Question_ID;
+  const { title, content } = req.body;
+
+  getConnection(function (conn) { 
+    const updateQuery = `
+      UPDATE Question
+      SET title = ?, content = ?
+      WHERE Question_ID = ?
+    `;
+
+    conn.query(
+      updateQuery,
+      [title, content, questionId],
+      function (error, results) {
+        conn.release();
+        if (error) {
+          console.error("Error executing query:", error);
+          res.status(500).json({ error: "An error occurred while updating the question." });
+          return;
+        }
+        res.json({ message: "Question updated successfully." });
+      }
+    );
+  });
+});
+
+router.delete("/questions/:Question_ID", (req, res) => {
+  const questionId = req.params.Question_ID;
+
+  getConnection(function (conn) {
+    const deleteQuery = "DELETE FROM Question WHERE Question_ID = ?";
+
+    conn.query(deleteQuery, [questionId], function (error, results) {
+      conn.release();
+      if (error) {
+        console.error("Error executing query:", error);
+        res.status(500).json({ error: "An error occurred while deleting the question." });
+        return;
+      }
+
+      res.json({ message: "Question deleted successfully." });
+    });
+  });
+});
+
+
+// 특정 질문에 대한 답변 추가 (user_ID가 "admin"일 경우에만)
+router.post("/questions/:Question_ID/:user_ID", (req, res) => {
+  const questionId = req.params.Question_ID;
+  const { Answer } = req.body;
+
+  // 세션에서 user_ID 확인
+  const user_ID = req.session.user.userID;
+  console.log(req.session.user.userID);
+
+  if (user_ID === "admin") {
+    getConnection(function (conn) {
+      const updateQuery = `
+        UPDATE Question
+        SET Answer = ?
+        WHERE Question_ID = ?
+      `;
+
+      conn.query(
+        updateQuery,
+        [Answer, questionId],
+        function (error, results) {
+          conn.release();
+          if (error) {
+            console.error("Error executing query:", error);
+            res.status(500).json({ error: "An error occurred while updating the question." });
+            return;
+          }
+
+          res.json({ message: "Answer added to the question." });
+        }
+      );
+    });
+  } else {
+    res.status(403).json({ error: "You do not have permission to add an answer." });
+  }
+});
+
+
+module.exports = router;
+
+/* // Edit Question
 router.post("/questions/:questionId", (req, res) => {
   const questions = JSON.parse(fs.readFileSync(questionFilePath, "utf8"));
   const { title, content } = req.body;
@@ -70,7 +251,9 @@ router.post("/questions/:questionId/:userID", (req, res) => {
 
   // 관리자인지 여부를 확인하는 로직 (예시로 "admin123"이 관리자의 userID라고 가정)
   if (userID !== "admin123") {
-    return res.status(403).json({ message: "Access denied. Only admin can add answer." });
+    return res
+      .status(403)
+      .json({ message: "Access denied. Only admin can add answer." });
   }
 
   const index = questions.findIndex((q) => q.id === parseInt(questionId));
@@ -85,7 +268,6 @@ router.post("/questions/:questionId/:userID", (req, res) => {
   res.json({ message: "Answer added successfully.", ...questions[index] });
 });
 
-
 // Delete Question
 router.delete("/questions/:questionId", (req, res) => {
   const questions = JSON.parse(fs.readFileSync(questionFilePath, "utf8"));
@@ -95,10 +277,6 @@ router.delete("/questions/:questionId", (req, res) => {
   fs.writeFileSync(questionFilePath, JSON.stringify(newQuestions));
 
   res.json({ message: "Question deleted successfully." });
-});
-
-
-
-
+}); */
 
 module.exports = router;
