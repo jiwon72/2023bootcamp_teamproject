228fb06ca31737d72d1a4a2e45a9f192a139e728
jinha <75898490+iububble@users.noreply.github.com>
2023-09-02 19:41:25 +0900

    fix : 즐겨찾기 불러오는 코드 output 수정

--- backend/routes/showfavorites.js
+++ backend/routes/showfavorites.js
@@ -2,53 +2,62 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const path = require("path");
-const getConnection=require("../data/DBpool")
+const getConnection=require("../data/DBpool");
+const { default: axios } = require("axios");
 
 const storagePath = path.join(__dirname, "../data");
 const userFilePath = path.join(storagePath, "favorite.json");
 
-// Function to read users from the JSON file
-const readFavorites = () => {
-  try {
-    const usersJson = fs.readFileSync(userFilePath, "utf8");
-    return JSON.parse(usersJson);
-  } catch (error) {
-    console.error("Error reading users from file:", error.message);
-    return [];
+//영화 ID목록를 통해 영화 정보들을 가져오는 함수
+const getMovieDetails = async (movieIds) => {
+  const movieDetails = [];
+  for (let id of movieIds) {
+    try {
+      const response = await axios.get(`http://localhost:3000/movies/${id}`);
+      movieDetails.push(response.data);
+    } catch (error) {
+      console.error(`Error fetching details for movie ID ${id}:`, error.message);
+    }
   }
-};
+  return movieDetails
+}
 
 // Get favorite route
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   const userSession = req.session.user;
   if (!userSession) {
     return res.status(401).json({ message: "Session expired. Please login." });
   }
   const userId=userSession.userID
   
-  // DB 연결
-  getConnection((conn) => {
-    // 해당 영화가 이미 즐겨찾기에 추가되어 있는지 확인하는 쿼리
-    const Findquery = "SELECT * FROM MoviesMark WHERE user_user_id = ?";
-    const userID = [userId];
+  try {
+    await getConnection(async (conn) => {
+      //DB에서 해당 유저의 즐겨찾기 목록을 가져옴
+      const Findquery = "SELECT * FROM MoviesMark WHERE user_user_id = ?";
+      const userID = [userId];
 
-    conn.query(Findquery, userID, (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send("An error occurred");
-        conn.release();
-        return;
-      }
-
-    
-    else {
-
-      res.json({favorites : result})
-
-
-    
-      }
+      conn.query(Findquery, userID, async (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send("An error occurred");
+          conn.release();
+          return;
+        } else {
+          //DB에서 가져온 영화ID 목록을 movieIdLst에 저장
+          const movieIdLst = result.map((movie) => movie.Movie_Movie_ID);
+          conn.release();
+          
+          // 영화 세부 정보 가져오기
+          const movieDetails = await getMovieDetails(movieIdLst);
+          res.status(200).json({favorites : movieDetails});
+        }
+      });
     });
-  });
+  } catch (err) {
+    console.error("Database error:", err.message);
+    res.status(500).send("An error occurred");
+  }
+
+
 });
 module.exports = router;
